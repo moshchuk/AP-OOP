b07fb5d6bf4ed7892055d03890b6b8436d50ffd9
moshchuk <sasuukeuchihaaa@gmail.com>
2025-03-15 01:03:12 +0200

    Update Lab1.8.cpp

--- Lab1.8.cpp
+++ Lab1.8.cpp
@@ -1,9 +1,92 @@
 //////////////////////////////////////////////////////////////////////////////
+// Triangle.h 
+// Заголовний файл 
+#ifndef TRIANGLE_H
+#define TRIANGLE_H
+
+#include <string>
+#include <cmath>
+#include <iostream>
+
+class Triangle {
+public:
+    class Point {
+    private:
+        double x, y;
+
+    public:
+        Point();
+        Point(double x, double y);
+        void Init(double x, double y);
+        void Read();
+        void Display() const;
+        std::string toString() const;
+        double getX() const;
+        double getY() const;
+        void setX(double x);
+        void setY(double y);
+        double distanceTo(const Point& other) const;
+    };
+
+private:
+    Point P1, P2, P3;
+
+public:
+    Triangle();
+    Triangle(const Point& p1, const Point& p2, const Point& p3);
+    void Init(const Point& p1, const Point& p2, const Point& p3);
+    void Read();
+    void Display() const;
+    std::string toString() const;
+    double getA() const;
+    double getB() const;
+    double getC() const;
+    double area() const;
+    double perimeter() const;
+    std::string triangleType() const;
+};
+
+#endif
+//////////////////////////////////////////////////////////////////////////////
 // Triangle.cpp 
 // Файл реалізації – реалізація методів класу
 #include "triangle.h"
-#include <iostream>
-#include <cmath>
+
+Triangle::Point::Point() : x(0), y(0) {}
+
+Triangle::Point::Point(double x, double y) : x(x), y(y) {}
+
+void Triangle::Point::Init(double x, double y) {
+    this->x = x;
+    this->y = y;
+}
+
+void Triangle::Point::Read() {
+    std::cout << "Введiть координату X: ";
+    std::cin >> x;
+    std::cout << "Введiть координату Y: ";
+    std::cin >> y;
+}
+
+void Triangle::Point::Display() const {
+    std::cout << "Точка: (" << x << ", " << y << ")" << std::endl;
+}
+
+std::string Triangle::Point::toString() const {
+    return "(" + std::to_string(x) + ", " + std::to_string(y) + ")";
+}
+
+double Triangle::Point::getX() const { return x; }
+
+double Triangle::Point::getY() const { return y; }
+
+void Triangle::Point::setX(double x) { this->x = x; }
+
+void Triangle::Point::setY(double y) { this->y = y; }
+
+double Triangle::Point::distanceTo(const Point& other) const {
+    return std::sqrt((x - other.x) * (x - other.x) + (y - other.y) * (y - other.y));
+}
 
 Triangle::Triangle() : P1(), P2(), P3() {}
 
@@ -16,11 +99,11 @@ void Triangle::Init(const Point& p1, const Point& p2, const Point& p3) {
 }
 
 void Triangle::Read() {
-    std::cout << "Введiть координати вершини 1:" << std::endl;
+    std::cout << "Введiть координати першої вершини:" << std::endl;
     P1.Read();
-    std::cout << "Введiть координати вершини 2:" << std::endl;
+    std::cout << "Введiть координати другої вершини:" << std::endl;
     P2.Read();
-    std::cout << "Введiть координати вершини 3:" << std::endl;
+    std::cout << "Введiть координати третьої вершини:" << std::endl;
     P3.Read();
 }
 
@@ -29,26 +112,11 @@ void Triangle::Display() const {
     P1.Display();
     P2.Display();
     P3.Display();
-
-    // Використання вкладеного класу для демонстрації
-    Vertex v1(P1), v2(P2), v3(P3);
-    std::cout << "Вершини за допомогою вкладеного класу:" << std::endl;
-    v1.Display();
-    v2.Display();
-    v3.Display();
 }
 
 std::string Triangle::toString() const {
-    return "Трикутник з вершинами: " + P1.toString() + ", " + P2.toString() + ", " + P3.toString();
+    return "Трикутник: " + P1.toString() + ", " + P2.toString() + ", " + P3.toString();
 }
-
-const Point& Triangle::getP1() const { return P1; }
-const Point& Triangle::getP2() const { return P2; }
-const Point& Triangle::getP3() const { return P3; }
-
-void Triangle::setP1(const Point& p1) { P1 = p1; }
-void Triangle::setP2(const Point& p2) { P2 = p2; }
-void Triangle::setP3(const Point& p3) { P3 = p3; }
 
 double Triangle::getA() const {
     return P2.distanceTo(P3);
@@ -60,21 +128,6 @@ double Triangle::getB() const {
 
 double Triangle::getC() const {
     return P1.distanceTo(P2);
-}
-
-double Triangle::getAngleA() const {
-    double a = getA(), b = getB(), c = getC();
-    return std::acos((b * b + c * c - a * a) / (2 * b * c));
-}
-
-double Triangle::getAngleB() const {
-    double a = getA(), b = getB(), c = getC();
-    return std::acos((a * a + c * c - b * b) / (2 * a * c));
-}
-
-double Triangle::getAngleC() const {
-    double a = getA(), b = getB(), c = getC();
-    return std::acos((a * a + b * b - c * c) / (2 * a * b));
 }
 
 double Triangle::area() const {
@@ -91,150 +144,11 @@ std::string Triangle::triangleType() const {
     double a = getA(), b = getB(), c = getC();
     if (a == b && b == c) return "Рiвностороннiй";
     if (a == b || b == c || a == c) return "Рiвнобедрений";
-    if (a * a == b * b + c * c || b * b == a * a + c * c || c * c == a * a + b * b) return "Прямокутний";
-    return "Iнший";
-}
-//////////////////////////////////////////////////////////////////////////////
-// Triangle.h 
-// Заголовний файл 
-#ifndef TRIANGLE_H
-#define TRIANGLE_H
-
-#include "point.h"
-
-class Triangle {
-private:
-    Point P1, P2, P3;
-
-    class Vertex {
-    private:
-        Point point;
-
-    public:
-        Vertex(const Point& p) : point(p) {}
-        void Display() const { point.Display(); }
-        const Point& getPoint() const { return point; }
-    };
-
-public:
-    Triangle();
-    Triangle(const Point& p1, const Point& p2, const Point& p3);
-    void Init(const Point& p1, const Point& p2, const Point& p3);
-    void Read();
-    void Display() const;
-    std::string toString() const;
-    const Point& getP1() const;
-    const Point& getP2() const;
-    const Point& getP3() const;
-    void setP1(const Point& p1);
-    void setP2(const Point& p2);
-    void setP3(const Point& p3);
-    double getA() const;
-    double getB() const;
-    double getC() const;
-    double getAngleA() const;
-    double getAngleB() const;
-    double getAngleC() const;
-    double area() const;
-    double perimeter() const;
-    std::string triangleType() const;
-};
-
-#endif
-//////////////////////////////////////////////////////////////////////////////
-// Point.h 
-// Заголовний файл 
-#ifndef POINT_H
-#define POINT_H
-
-#include <string>
-#include <cmath>
-
-class Point {
-private:
-    double x, y;
-
-public:
-    Point();
-    Point(double x, double y);
-    void Init(double x, double y);
-    void Read();
-    void Display() const;
-    std::string toString() const;
-    double getX() const;
-    double getY() const;
-    void setX(double x);
-    void setY(double y);
-    void moveX(double dx);
-    void moveY(double dy);
-    double distanceToOrigin() const;
-    double distanceTo(const Point& other) const;
-    void toPolar(double& r, double& phi) const;
-    bool operator==(const Point& other) const;
-    bool operator!=(const Point& other) const;
-};
-
-#endif
-//////////////////////////////////////////////////////////////////////////////
-// Point.cpp 
-// Файл реалізації – реалізація методів класу
-
-#include "point.h"
-#include <iostream>
-#include <cmath>
-
-Point::Point() : x(0), y(0) {}
-
-Point::Point(double x, double y) : x(x), y(y) {}
-
-void Point::Init(double x, double y) {
-    this->x = x;
-    this->y = y;
-}
-
-void Point::Read() {
-    std::cout << "Введiть координату X: ";
-    std::cin >> x;
-    std::cout << "Введiть координату Y: ";
-    std::cin >> y;
-}
-
-void Point::Display() const {
-    std::cout << "Точка: (" << x << ", " << y << ")" << std::endl;
-}
-
-std::string Point::toString() const {
-    return "Точка: (" + std::to_string(x) + ", " + std::to_string(y) + ")";
-}
-
-double Point::getX() const { return x; }
-double Point::getY() const { return y; }
-
-void Point::setX(double x) { this->x = x; }
-void Point::setY(double y) { this->y = y; }
-
-void Point::moveX(double dx) { x += dx; }
-void Point::moveY(double dy) { y += dy; }
-
-double Point::distanceToOrigin() const {
-    return std::sqrt(x * x + y * y);
-}
-
-double Point::distanceTo(const Point& other) const {
-    return std::sqrt((x - other.x) * (x - other.x) + (y - other.y) * (y - other.y));
-}
-
-void Point::toPolar(double& r, double& phi) const {
-    r = distanceToOrigin();
-    phi = std::atan2(y, x);
-}
-
-bool Point::operator==(const Point& other) const {
-    return (x == other.x && y == other.y);
-}
-
-bool Point::operator!=(const Point& other) const {
-    return !(*this == other);
+    if (std::abs(a * a + b * b - c * c) < 1e-6 ||
+        std::abs(a * a + c * c - b * b) < 1e-6 ||
+        std::abs(b * b + c * c - a * a) < 1e-6)
+        return "Прямокутний";
+    return "Загальний";
 }
 //////////////////////////////////////////////////////////////////////////////
 // Lab1.8.cpp 
@@ -244,6 +158,7 @@ bool Point::operator!=(const Point& other) const {
 
 int main() {
     setlocale(LC_CTYPE, "ukr");
+
     Triangle triangle;
     triangle.Read();
     triangle.Display();
